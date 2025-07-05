7b89189a64656f30bb37619c8ffbf5f98515286f
hemahemapathi <167777771+hemahemapathi@users.noreply.github.com>
2025-07-05 11:41:32 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://project-management-tool-jj3f.onrender.com',
   credentials: true
 }));
 
